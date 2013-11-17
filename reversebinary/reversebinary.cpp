76fd26a55cd99b709e2a44ca1abfd90b661a404d
Carl-Anton Ingmarsson <mail@carlanton.se>
2013-11-17 21:53:28 +0100

    Reversed Binary: comment

--- reversebinary/reversebinary.cpp
+++ reversebinary/reversebinary.cpp
@@ -5,14 +5,19 @@ int main(int argc, char* argv[])
   unsigned int N, Nr;
 
   std::cin >> N;
+
+  // Reads rightmost bit in N and appends it to Nr.
   Nr = 0;
   while (N != 0) {
+    // Shift previous written bits to the left
     Nr <<= 1;
+    // "Append" bit
     Nr |= N & 0x1;
+    // Shift away just read bit
     N >>= 1;
   }
 
- std::cout << Nr << std::endl;
+  std::cout << Nr << std::endl;
 
   return 0;
 }
